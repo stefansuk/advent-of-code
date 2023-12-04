3e8cb4dc300ca320f648748623febcca3ed9d6ee
stefansuk <stefan@suk.frl>
2023-12-04 01:53:39 +0100

    Day 2 part 2

--- 2023/Day2Part2.java
+++ 2023/Day2Part2.java
@@ -1,8 +1,6 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,31 +9,21 @@ public class Day2Part2 {
     public static void main(String[] args) throws IOException {
         String input = Files.readString(Path.of("2023/Day2Input"));
         List<String> lines = input.lines().toList();
-        Pattern p = Pattern.compile("\\d+ (red|green|blue)");
-        record Pair(int amount, String colour) {}
+        Pattern p = Pattern.compile("(\\d+) (red|green|blue)");
         int sum = 0;
         for (String line : lines) {
+            int maxRedCubes = 0, maxGreenCubes = 0, maxBlueCubes = 0;
             Matcher m = p.matcher(line);
-            List<Pair> pairs = new ArrayList<>();
             while (m.find()) {
-                String[] amountColourPair = m.group().split(" ");
-                pairs.add(new Pair(Integer.parseInt(amountColourPair[0]), amountColourPair[1]));
+                int amount = Integer.parseInt(m.group(1));
+                String colour = m.group(2);
+                if (colour.equals("red") && amount > maxRedCubes)
+                    maxRedCubes = amount;
+                if (colour.equals("green") && amount > maxGreenCubes)
+                    maxGreenCubes = amount;
+                if (colour.equals("blue") && amount > maxBlueCubes)
+                    maxBlueCubes = amount;
             }
-            int maxRedCubes = Collections.max(pairs.stream()
-                    .filter(pair -> pair.colour.equals("red"))
-                    .mapToInt(pair -> pair.amount)
-                    .boxed()
-                    .toList());
-            int maxGreenCubes = Collections.max(pairs.stream()
-                    .filter(pair -> pair.colour.equals("green"))
-                    .mapToInt(pair -> pair.amount)
-                    .boxed()
-                    .toList());
-            int maxBlueCubes = Collections.max(pairs.stream()
-                    .filter(pair -> pair.colour.equals("blue"))
-                    .mapToInt(pair -> pair.amount)
-                    .boxed()
-                    .toList());
             sum += maxRedCubes * maxGreenCubes * maxBlueCubes;
         }
         System.out.println(sum);
