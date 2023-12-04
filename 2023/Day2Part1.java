a8cba051abb8bde3c9ce61123592bc59c7183bcf
stefansuk <stefan@suk.frl>
2023-12-04 01:48:40 +0100

    Day 2 part 1

--- 2023/Day2Part1.java
+++ 2023/Day2Part1.java
@@ -9,12 +9,12 @@ public class Day2Part1 {
     public static void main(String[] args) throws IOException {
         String input = Files.readString(Path.of("2023/Day2Input"));
         List<String> lines = input.lines().toList();
-        Pattern p = Pattern.compile("\\d([3-9] red|[4-9] green|[5-9] blue)");
+        Pattern p = Pattern.compile("^(?:(?!\\d([3-9] red|[4-9] green|[5-9] blue)).)+$");
         int sum = 0;
         for (int i = 0; i < lines.size(); i++) {
             String line = lines.get(i);
             Matcher m = p.matcher(line);
-            if (!m.find())
+            if (m.find())
                 sum += i + 1;
         }
         System.out.println(sum);
