3c6fd91e6c7cc10075b1b40f941d0d96fadc249f
stefansuk <stefan@suk.frl>
2023-12-09 18:16:41 +0100

    Day 6 part 2

--- 2023/Day6Part2.java
+++ 2023/Day6Part2.java
@@ -1,14 +1,13 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
 public class Day6Part2 {
     public static void main(String[] args) throws IOException {
         String input = Files.readString(Path.of("2023/Day6Input"));
-        List<String> lines = input.lines().toList();
-        long time = Integer.parseInt(lines.get(0).substring(13).replaceAll("\\D+", ""));
-        long distance = Long.parseLong(lines.get(1).substring(12).replaceAll("\\D+", ""));
+        String[] lines = input.lines().toArray(String[]::new);
+        long time = Integer.parseInt(lines[0].substring(13).replaceAll("\\D+", ""));
+        long distance = Long.parseLong(lines[1].substring(12).replaceAll("\\D+", ""));
         long lowest = ((int) (time - Math.sqrt(time * time - 4 * distance)) >> 1) + 1;
         long winners = time - (lowest << 1) + 1;
         System.out.println(winners);
