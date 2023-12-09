0394ed9689c11a0bcd5e93fddb11772a00bdbc03
stefansuk <stefan@suk.frl>
2023-12-09 18:13:29 +0100

    Day 1 part 1

--- 2023/Day1Part1.java
+++ 2023/Day1Part1.java
@@ -1,12 +1,11 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
 public class Day1Part1 {
     public static void main(String[] args) throws IOException {
         String input = Files.readString(Path.of("2023/Day1Input"));
-        List<String> lines = input.lines().toList();
+        String[] lines = input.lines().toArray(String[]::new);
         int sum = 0;
         for (String line : lines) {
             char first = '\0', last = '\0';
