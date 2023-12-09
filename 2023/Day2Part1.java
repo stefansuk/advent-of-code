66c01c444d6f41b035810ef41c432529058ed0ac
stefansuk <stefan@suk.frl>
2023-12-09 18:13:58 +0100

    Day 2 part 1

--- 2023/Day2Part1.java
+++ 2023/Day2Part1.java
@@ -1,18 +1,17 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Day2Part1 {
     public static void main(String[] args) throws IOException {
         String input = Files.readString(Path.of("2023/Day2Input"));
-        List<String> lines = input.lines().toList();
+        String[] lines = input.lines().toArray(String[]::new);
         Pattern p = Pattern.compile("^(?:(?!\\d([3-9] red|[4-9] green|[5-9] blue)).)+$");
         int sum = 0;
-        for (int i = 0; i < lines.size(); i++) {
-            String line = lines.get(i);
+        for (int i = 0; i < lines.length; i++) {
+            String line = lines[i];
             Matcher m = p.matcher(line);
             if (m.find())
                 sum += i + 1;
