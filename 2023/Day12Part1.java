4054d928327a458f091d4a2ab27026e27f0718dc
stefansuk <stefan@suk.frl>
2023-12-14 20:51:07 +0100

    Day 12 part 1

--- 2023/Day12Part1.java
+++ 2023/Day12Part1.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -22,47 +21,26 @@ public class Day12Part1 {
                 conditionRecord = m1.group();
             if (m2.find())
                 damagedSpringGroups = Arrays.stream(m2.group().split(",")).mapToInt(Integer::parseInt).toArray();
-            ArrayList<ArrayList<Integer>> operationalPossibilities = findOperationalPossibilities(new ArrayList<>(), damagedSpringGroups.length, true, conditionRecord.length() - Arrays.stream(damagedSpringGroups).sum());
-            for (ArrayList<Integer> operationalPossibility : operationalPossibilities) {
-                StringBuilder stringBuilder = new StringBuilder();
-                for (int i = 0; i < damagedSpringGroups.length; i++) {
-                    stringBuilder.append(".".repeat(operationalPossibility.get(i)));
-                    stringBuilder.append("#".repeat(damagedSpringGroups[i]));
-                }
-                stringBuilder.append(".".repeat(operationalPossibility.getLast()));
-                sum = checkArrangement(stringBuilder.toString().toCharArray(), conditionRecord.toCharArray()) ? sum + 1 : sum;
-            }
+            sum += findPossibleArrangements(conditionRecord, damagedSpringGroups);
         }
         System.out.println(sum);
     }
 
-    public static ArrayList<ArrayList<Integer>> findOperationalPossibilities(ArrayList<ArrayList<Integer>> operationalPossibilities, int damagedSpringGroupsCount, boolean isFirst, int totalOperational) {
-        if (isFirst) {
-            for (int i = 0; i <= totalOperational - damagedSpringGroupsCount + 1; i++) {
-                operationalPossibilities.add(new ArrayList<>());
-                operationalPossibilities.getLast().add(i);
-                findOperationalPossibilities(operationalPossibilities, damagedSpringGroupsCount - 1, false, totalOperational - i);
-            }
-            return operationalPossibilities;
-        }
-        if (damagedSpringGroupsCount == 0) {
-            operationalPossibilities.getLast().add(totalOperational);
-            return operationalPossibilities;
-        }
-        ArrayList<Integer> currentPossibility = new ArrayList<>(operationalPossibilities.getLast());
-        for (int i = 1; i <= totalOperational - damagedSpringGroupsCount + 1; i++) {
-            if (i != 1)
-                operationalPossibilities.add(new ArrayList<>(currentPossibility));
-            operationalPossibilities.getLast().add(i);
-            findOperationalPossibilities(operationalPossibilities, damagedSpringGroupsCount - 1, false, totalOperational - i);
-        }
-        return operationalPossibilities;
-    }
-
-    public static boolean checkArrangement(char[] arrangement, char[] conditionRecord) {
-        for (int i = 0; i < arrangement.length; i++)
-            if (arrangement[i] != conditionRecord[i] && conditionRecord[i] != '?')
-                return false;
-        return true;
+    private static int findPossibleArrangements(String conditionRecord, int[] damagedSpringGroups) {
+        if (damagedSpringGroups.length == 0 && conditionRecord.contains("#") || Arrays.stream(damagedSpringGroups).sum() + damagedSpringGroups.length - 1 > conditionRecord.length())
+            return 0;
+        if (damagedSpringGroups.length == 0)
+            return 1;
+        if (conditionRecord.charAt(0) == '.')
+            return findPossibleArrangements(conditionRecord.substring(1), damagedSpringGroups);
+        int possibleArrangements = 0;
+        if (conditionRecord.length() == damagedSpringGroups[0]
+                && conditionRecord.substring(0, damagedSpringGroups[0]).matches("[#?]+"))
+            possibleArrangements += findPossibleArrangements(conditionRecord.substring(damagedSpringGroups[0]), Arrays.copyOfRange(damagedSpringGroups, 1, damagedSpringGroups.length));
+        if (conditionRecord.length() > damagedSpringGroups[0] && conditionRecord.substring(0, damagedSpringGroups[0] + 1).matches("[#?]+[.?]"))
+            possibleArrangements += findPossibleArrangements(conditionRecord.substring(damagedSpringGroups[0] + 1), Arrays.copyOfRange(damagedSpringGroups, 1, damagedSpringGroups.length));
+        if (conditionRecord.charAt(0) == '?')
+            possibleArrangements += findPossibleArrangements(conditionRecord.substring(1), damagedSpringGroups);
+        return possibleArrangements;
     }
 }
