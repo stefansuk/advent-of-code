6b22ed0d76aeac99aa4798b564cb170f3c648819
stefansuk <stefan@suk.frl>
2023-12-16 10:36:59 +0100

    Day 15 part 1

--- 2023/Day15Part1.java
+++ 2023/Day15Part1.java
@@ -5,11 +5,11 @@ import java.nio.file.Path;
 public class Day15Part1 {
     public static void main(String[] args) throws IOException {
         String input = Files.readString(Path.of("2023/Day15Input"));
-        String[] codes = input.split(",");
+        String[] steps = input.split(",");
         int sum = 0;
-        for (String code : codes) {
+        for (String step : steps) {
             int currentValue = 0;
-            for (char c : code.toCharArray()) {
+            for (char c : step.toCharArray()) {
                 currentValue += c;
                 currentValue *= 17;
                 currentValue %= 256;
