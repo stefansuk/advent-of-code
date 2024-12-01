bd18e037e092de488d4b0334f7fecaeb443b2d23
stefansuk <stefan@suk.frl>
2024-12-01 11:46:39 +0100

    Day 1 part 1

---
+++ 2024/Day1Part1.clj
@@ -0,0 +1,8 @@
+(def input (slurp "2024/Day1Input"))
+(def lines (re-seq #"(\d+)\s+(\d+)" input))
+(def lists (apply map vector (map (fn [x] (rest x)) lines)))
+(def distances (map abs (map -
+                             (map #(Integer/parseInt %) (sort (first lists)))
+                             (map #(Integer/parseInt %) (sort (last lists))))))
+(def sum (apply + distances))
+(print sum)
