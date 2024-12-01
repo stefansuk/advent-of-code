ac6a85f8ecbf3758d18dbd8dae1240f71ec56158
stefansuk <stefan@suk.frl>
2024-12-01 13:05:35 +0100

    Day 1 part 2

---
+++ 2024/Day1Part2.clj
@@ -0,0 +1,9 @@
+(def input (slurp "2024/Day1Input"))
+(def lines (re-seq #"(\d+)\s+(\d+)" input))
+(def lists (apply map vector (map (fn [x] (rest x)) lines)))
+(def products (map (fn [x] (*
+                             (#(Integer/parseInt %) x)
+                             (get (frequencies (last lists)) x 0)))
+                   (first lists)))
+(def sum (apply + products))
+(print sum)
