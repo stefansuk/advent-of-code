73881889ab53b2f81f87052703e075bc646b9339
stefansuk <stefan@suk.frl>
2024-12-03 10:09:07 +0100

    Day 3 part 2

---
+++ 2024/Day3Part2.clj
@@ -0,0 +1,8 @@
+(require '[clojure.string :as str])
+
+(def input (slurp "2024/Day3Input"))
+(def sum (apply + (map #(*
+                          (Integer/parseInt (get % 1))
+                          (Integer/parseInt (get % 2)))
+                       (re-seq #"mul\((\d+),(\d+)\)" (str/replace input #"don't\(\)(?s).*?(?:do\(\)|\z)" "")))))
+(print sum)
