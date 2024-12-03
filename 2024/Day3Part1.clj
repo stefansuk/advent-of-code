a6f1f6b9a168317d9ea56a922049242ded797184
stefansuk <stefan@suk.frl>
2024-12-03 08:20:45 +0100

    Day 3 part 1

---
+++ 2024/Day3Part1.clj
@@ -0,0 +1,6 @@
+(def input (slurp "2024/Day3Input"))
+(def sum (apply + (map #(*
+                          (Integer/parseInt (get % 1))
+                          (Integer/parseInt (get % 2)))
+                       (re-seq #"mul\((\d+),(\d+)\)" input))))
+(print sum)
