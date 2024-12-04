292a1a85ada75f3bce837c6c4e59f56e4ba6726f
stefansuk <stefan@suk.frl>
2024-12-04 16:13:21 +0100

    Day 4 part 1

--- 2024/Day4Part1.clj
+++ 2024/Day4Part1.clj
@@ -2,14 +2,14 @@
 
 (def input (slurp "2024/Day4Input"))
 (def word "XMAS")
-(def width (- (count (re-find #".*\n" input)) 1))
+(def width (dec (count (re-find #".*\n" input))))
 (def grid (partition width (str/replace input #"\n" "")))
 (def height (count grid))
 (def directions [[0 1] [1 1] [1 0] [-1 1] [-1 0] [-1 -1] [0 -1] [1 -1]])
 (def total (count (for [y (range height)
                         x (range width)
                         d directions
-                        :when (let [w (- (count word) 1)]
+                        :when (let [w (dec (count word))]
                                 (and (>= (+ x (* (first d) w)) 0)
                                      (< (+ x (* (first d) w)) width)
                                      (>= (+ y (* (last d) w)) 0)
