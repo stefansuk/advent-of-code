60c1692e8003d7ec9c1d62a4e315120fdcd0c364
stefansuk <stefan@suk.frl>
2024-12-05 13:19:51 +0100

    Day 5 part 1

--- 2024/Day5Part1.clj
+++ 2024/Day5Part1.clj
@@ -2,9 +2,9 @@
 
 (def input (slurp "2024/Day5Input"))
 (def numbers (->> input
-                 (#(str/split % #"\n\n"))
-                 (map #(str/split % #"\n"))
-                 (map (fn [x] (map #(str/split % #"\||,") x)))))
+                  (#(str/split % #"\n\n"))
+                  (map #(str/split % #"\n"))
+                  (map (fn [x] (map #(str/split % #"\||,") x)))))
 (def rules (set (first numbers)))
 (def updates (second numbers))
 (def sum (apply + (keep (fn [x] (when
