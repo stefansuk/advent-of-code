64345d6389f1927f896694b1177d9347988b5f54
stefansuk <stefan@suk.frl>
2024-12-06 17:47:19 +0100

    Day 6 part 1

--- 2024/Day6Part1.clj
+++ 2024/Day6Part1.clj
@@ -11,24 +11,24 @@
                 [col row])))
 (def path (assoc grid (c 1) (assoc (grid (c 1)) (c 0) \X)))
 (def visited (loop [p path
-                 x (get c 0)
-                 y (get c 1)
-                 d directions]
-            (let [dx (get-in d [0 0])
-                  dy (get-in d [0 1])]
-              (if (and (>= (+ x dx) 0)
-                       (>= (+ y dy) 0)
-                       (< (+ x dx) width)
-                       (< (+ y dy) height))
-                (if (= \# (get-in grid [(+ y dy) (+ x dx)]))
-                  (recur p
-                         x
-                         y
-                         (into (vec (rest d)) [(first d)]))
-                  (recur (assoc p (+ y dy) (assoc (p (+ y dy)) (+ x dx) \X))
-                         (+ x dx)
-                         (+ y dy)
-                         d))
-                p))))
+                    x (get c 0)
+                    y (get c 1)
+                    d directions]
+               (let [dx (get-in d [0 0])
+                     dy (get-in d [0 1])]
+                 (if (and (>= (+ x dx) 0)
+                          (>= (+ y dy) 0)
+                          (< (+ x dx) width)
+                          (< (+ y dy) height))
+                   (if (= \# (get-in grid [(+ y dy) (+ x dx)]))
+                     (recur p
+                            x
+                            y
+                            (into (vec (rest d)) [(first d)]))
+                     (recur (assoc p (+ y dy) (assoc (p (+ y dy)) (+ x dx) \X))
+                            (+ x dx)
+                            (+ y dy)
+                            d))
+                   p))))
 (def total (count (filter #(= \X %) (flatten visited))))
 (println total)
