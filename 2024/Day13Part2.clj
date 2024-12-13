d316b6dcd1cf7f23c510d9743771495877be0844
stefansuk <stefan@suk.frl>
2024-12-13 21:02:15 +0100

    Day 13 part 2

---
+++ 2024/Day13Part2.clj
@@ -0,0 +1,19 @@
+(require '[clojure.string :as str])
+
+(def input (slurp "2024/Day13Input"))
+(def values (->> (str/split input #"\n\n")
+                 (mapv #(re-seq #"\d+" %))
+                 (mapv (fn [x] (mapv #(Integer/parseInt %) x)))
+                 (mapv #(update % 4 + 10000000000000))
+                 (mapv #(update % 5 + 10000000000000))))
+(def total (reduce + (for [v values
+                           :let [a (/ (- (* (v 3) (v 4))
+                                         (* (v 2) (v 5)))
+                                      (- (* (v 0) (v 3))
+                                         (* (v 1) (v 2))))]
+                           :when (integer? a)
+                           :let [b (/ (- (v 4)
+                                         (* (v 0) a))
+                                      (v 2))]]
+                       (+ (* 3 a) b))))
+(println total)
